8a3a8049eedcdb0fd0c901f1528fb39fb7de3fd5
ReddVee <andrey7306iva9254@gmail.com>
2026-09-12 00:30:23 +0300

    Add files via upload

---
+++ 6/Practicum (4).java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+//237 группа
+//Задание 1
+public class Practicum {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.drawCircle();
+        Square square = new Square();
+        square.drawSquare();
+        Rectangle rectangle = new Rectangle();
+        rectangle.drawRectangle();
+        Ellipse ellipse = new Ellipse();
+        ellipse.drawCircle();
+        ellipse.circleToEllipse();
+    }
+}
+
+class Figure {
+    public void drawCircle() {
+        System.out.println("Рисуем круг");
+    }
+
+    public void drawSquare() {
+        System.out.println("Рисуем квадрат");
+    }
+
+    public void drawRectangle() {
+        System.out.println("Рисуем прямоугольник");
+    }
+}
+
+class Circle extends Figure{
+}
+
+class Square extends Figure{
+}
+
+class Rectangle extends Figure{
+}
+
+class Ellipse extends Figure{
+    public void circleToEllipse() {
+        System.out.println("Превращаем круг в овал");
+    }
+}
